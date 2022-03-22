7f81ac8728d2b5e68dd0b91440d4d55b64d1985b
leonardo <leonardomilov@gmail.com>
2022-03-22 18:03:48 -0300

    definindo os atributos de meta na models

--- fgagenda/meta/admin.py
+++ fgagenda/meta/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from fgagenda.meta.models import Meta
+
 # Register your models here.
+admin.site.register(Meta)
--- fgagenda/meta/models.py
+++ fgagenda/meta/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.core import validators
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
+from fgagenda.todolist.models import ToDoList
+
+
+class Meta(models.Model):
+    
+    class Meta:
+        verbose_name = _("Meta")
+        verbose_name_plural = _("Metas")
+
+    todolist = models.OneToOneField(
+        ToDoList,
+        on_delete=models.RESTRICT,
+        related_name="todolist",
+        verbose_name = _("Lista de Tarefa"),
+        editable=True,
+        blank=False,
+        null=True
+    )
+
+    nome = models.CharField(
+        verbose_name=_("Nome"),
+        unique = True,
+        max_length=50,
+        blank=False
+    )
+
+    descricao = models.TextField(
+        verbose_name=_("Descrição"),
+        max_length=200,
+        blank=True,
+        null=True
+    )
+
+    dataInicio = models.DateTimeField(
+        verbose_name=_("Data Inicio"),
+        default=timezone.now
+    )
+
+    dataFim = models.DateTimeField(
+        verbose_name=_("Data Fim"),
+        blank=True
+    )
+
+    def configura_data_inicio(self):
+        self.dataInicio = timezone.now()
+        self.save()
